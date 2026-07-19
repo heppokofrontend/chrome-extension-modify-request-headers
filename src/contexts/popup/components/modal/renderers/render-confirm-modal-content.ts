f1af70c6f936a4a78fac4aa3096b956a7013950e
heppokofrontend <heppokofrontend@gmail.com>
2026-07-20 00:29:34 +0900

    Refactor renderConfirmModalContent data branching to a switch

--- src/contexts/popup/components/modal/renderers/render-confirm-modal-content.ts
+++ src/contexts/popup/components/modal/renderers/render-confirm-modal-content.ts
@@ -12,22 +12,26 @@ export const renderConfirmModalContent = ({ message, data }: Params) => {
   UI.modalButtonsContainer.replaceChildren();
   UI.modalData.replaceChildren();
 
-  if (data === undefined) {
-    return;
+  switch (typeof data) {
+    case 'string': {
+      const p = document.createElement('p');
+
+      p.textContent = data;
+      UI.modalData.append(p);
+      return;
+    }
+
+    case 'object': {
+      const asIs = document.createElement('p');
+      const toBe = document.createElement('p');
+
+      asIs.textContent = `${getMessage('modal_asIs')}: ${data.asIs}`;
+      toBe.textContent = `${getMessage('modal_toBe')}: ${data.toBe}`;
+      UI.modalData.append(asIs, toBe);
+      return;
+    }
+
+    default:
+      return;
   }
-
-  if (typeof data === 'string') {
-    const p = document.createElement('p');
-
-    p.textContent = data;
-    UI.modalData.append(p);
-    return;
-  }
-
-  const asIs = document.createElement('p');
-  const toBe = document.createElement('p');
-
-  asIs.textContent = `${getMessage('modal_asIs')}: ${data.asIs}`;
-  toBe.textContent = `${getMessage('modal_toBe')}: ${data.toBe}`;
-  UI.modalData.append(asIs, toBe);
 };
